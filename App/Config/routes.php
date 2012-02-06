dbdff14bf139480fc84059adccb63647c568d4bd
Paul Dragoonis <dragoonis@gmail.com>
2012-02-06 18:30:20 +0000

    Adding new routes

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -1,9 +1,22 @@
 <?php
 $routes = array();
-$routes['__default__'] = 'conference/index';
+
+$routes['__default__'] = 'home/index';
 $routes['__404__'] = 'general/show404';
 
-$routes['/account'] = 'user/showaccount';
-$routes['/account/view/(:any)'] = 'user/showaccount/username/$1';
-$routes['/account/edit'] = 'user/editaccount';
+$routes['/my/talks'] = 'talk/my';
+
+$routes['/account']               = 'user/showaccount';
+$routes['/account/view/(:any)']   = 'user/showaccount/username/$1';
+$routes['/account/edit']          = 'user/editaccount';
 $routes['/account/edit/password'] = 'user/editpassword';
+
+$routes['/manage/users/create']        = 'manage/createuser';
+$routes['/manage/users/view/(:num)']   = 'manage/viewuser/$1';
+$routes['/manage/users/edit/(:num)']   = 'manage/edituser/$1';
+$routes['/manage/users/delete/(:num)'] = 'manage/deleteuser/$1';
+
+$routes['/manage/talks/create']        = 'manage/createtalk';
+$routes['/manage/talks/view/(:num)']   = 'manage/viewtalk/$1';
+$routes['/manage/talks/edit/(:num)']   = 'manage/edittalk/$1';
+$routes['/manage/talks/delete/(:num)'] = 'manage/deletetalk/$1';
