dbd56f9973207e83dd3dc7cc07187ed635479b50
Paul Dragoonis <dragoonis@gmail.com>
2012-02-11 15:03:21 +0000

    Updating method name to get speaker names with votes

--- App/Controller/Vote.php
+++ App/Controller/Vote.php
@@ -4,7 +4,7 @@ class Vote extends Application {
 
 	function index() {
 		
-		$talks = $this->getTalkStorage()->getAllForVotes();
+		$talks = $this->getTalkStorage()->getAllWithSpeakerName();
 
 		$this->addCSS('jquery-ui/jquery-ui-1.8.17.custom', 'vote/index');
 		$this->addJS('libs/jquery-ui-1.8.17.custom.min', 'app/vote/index');
