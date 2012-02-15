83840e53df5f103d5a2ba88cc5bdb70b60c51e61
Paul Dragoonis <dragoonis@gmail.com>
2012-02-15 17:07:53 +0000

    Removing an obsolete view

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -7,7 +7,6 @@ $routes['__404__'] = 'general/show404';
 $routes['/my/talks'] = 'talk/my';
 
 $routes['/account']               = 'user/showaccount';
-$routes['/account/view/(:any)']   = 'user/showaccount/username/$1';
 $routes['/account/edit']          = 'user/editaccount';
 $routes['/account/edit/password'] = 'user/editpassword';
 
